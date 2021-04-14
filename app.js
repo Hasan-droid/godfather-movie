59e48bf35fafecc646ccbf48271f348d05615dfe
Hasan1122 <>
2021-04-14 11:39:38 +0000

    from replit

---
+++ app.js
@@ -0,0 +1,9 @@
+var sesion=prompt("which seesion would like to know about")
+if(sesion == 2)
+{
+  document.getElementById("img01").src="https://images-na.ssl-images-amazon.com/images/I/51eL8bPdkrL._SY445_.jpg"
+}
+else if (sesion== 3)
+{
+  document.getElementById("img01").src="https://upload.wikimedia.org/wikipedia/en/5/55/GodfatherIII2.jpg"
+}
